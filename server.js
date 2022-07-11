28225e77d32566cb72da996386762b5a7ea7a535
Foysal Hasan <foysalhasan499@gmail.com>
2022-07-11 11:01:32 +0600

    init-x

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const http = require('http')
 const app = require('./app/app')
 
 const PORT = process.env.PORT || 8080
-
 const server = http.createServer(app)
 server.listen(PORT, () => console.log(`SERVER IS RUNNING ON ${PORT} PORT`))
